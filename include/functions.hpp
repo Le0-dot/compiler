8d96ee03a4e4060b8a0038f8dac27b3015757bae
Le0-dot <lkolyadich@gmail.com>
2023-09-04 17:18:31 +0200

    Refactor special functions.

--- include/functions.hpp
+++ include/functions.hpp
@@ -4,6 +4,7 @@
 #include <unordered_map>
 #include <utility>
 #include <functional>
+#include <optional>
 
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Value.h>
@@ -12,34 +13,69 @@
 #include "type/type_id.hpp"
 
 
-class special_functions {
+class casts {
 public:
-    using cast_function_inserter = llvm::Value*(llvm::IRBuilderBase*, llvm::Value*);
-    using unary_function_inserter = llvm::Value*(llvm::IRBuilderBase*, llvm::Value*);
-    using binary_function_inserter = llvm::Value*(llvm::IRBuilderBase*, llvm::Value*, llvm::Value*);
+    using cast_inserter = llvm::Value*(llvm::IRBuilderBase*, llvm::Value*);
+    using inserter_wrapper = std::function<cast_inserter>;
+    
+private:
+    std::unordered_map<type::type_id, inserter_wrapper> _casts{};
 
-    using cast_inserter_wrapper = std::function<cast_function_inserter>;
-    using unary_inserter_wrapper = std::function<unary_function_inserter>;
-    using binary_inserter_wrapper = std::function<binary_function_inserter>;
+public:
+    auto get(type::type_id to_type) const -> std::optional<inserter_wrapper>;
+    void insert(type::type_id to_type, inserter_wrapper inserter);
+};
+
+class unary_operator {
+public:
+    using unary_inserter = llvm::Value*(llvm::IRBuilderBase*, llvm::Value*);
+    using inserter_wrapper = std::function<unary_inserter>;
 
 private:
-    using unary_variation = std::unordered_map<type::type_id, unary_inserter_wrapper>;
-    using binary_variation = std::unordered_map<std::pair<type::type_id, type::type_id>, binary_inserter_wrapper, pair_hash<type::type_id, type::type_id>>;
-
-    std::unordered_map<std::pair<type::type_id, type::type_id>, cast_inserter_wrapper, pair_hash<type::type_id, type::type_id>> _casts{};
-    std::unordered_map<std::string, unary_variation> _unary{};
-    std::unordered_map<std::string, binary_variation> _binary{};
+    using variation = std::unordered_map<type::type_id, inserter_wrapper>;
+     
+    std::uint64_t _precedense{};
+    variation _unary{};
 
 public:
-    auto cast_exists(type::type_id from_t, type::type_id to_t) const noexcept -> bool;
-    auto cast(type::type_id from_t, type::type_id to_t) const noexcept -> cast_inserter_wrapper;
-    void insert_cast(type::type_id from_t, type::type_id to_t, cast_inserter_wrapper inserter) noexcept;
+    inline explicit unary_operator(std::uint64_t precedense) : _precedense{precedense} {}
 
-    auto unary_exists(const std::string& oper, type::type_id tid) const noexcept -> bool;
-    auto unary(const std::string& oper, type::type_id tid) const noexcept -> unary_inserter_wrapper;
-    void insert_unary(const std::string& oper, type::type_id tid, unary_inserter_wrapper inserter) noexcept;
+    inline auto precedense() const noexcept -> std::uint64_t { return _precedense; }
 
-    auto binary_exists(const std::string& oper, type::type_id left, type::type_id right) const noexcept -> bool;
-    auto binary(const std::string& oper, type::type_id left, type::type_id right) const noexcept -> binary_inserter_wrapper;
-    void insert_binary(const std::string& oper, type::type_id left, type::type_id right, binary_inserter_wrapper inserter) noexcept;
+    auto get(type::type_id operand_type) -> std::optional<inserter_wrapper>;
+    void insert(type::type_id operand_type, inserter_wrapper inserter);
+};
+
+class binary_operator {
+public:
+    using binary_inserter = llvm::Value*(llvm::IRBuilderBase*, llvm::Value*, llvm::Value*);
+    using inserter_wrapper = std::function<binary_inserter>;
+
+private:
+    using variation = std::unordered_map<std::pair<type::type_id, type::type_id>, inserter_wrapper, pair_hash<type::type_id, type::type_id>>;
+     
+    std::uint64_t _precedense{};
+    variation _binary{};
+
+public:
+    inline explicit binary_operator(std::uint64_t precedense) : _precedense{precedense} {}
+
+    inline auto precedense() const noexcept -> std::uint64_t { return _precedense; }
+
+    auto get(type::type_id left, type::type_id right) -> std::optional<inserter_wrapper>;
+    void insert(type::type_id left, type::type_id right, inserter_wrapper inserter);
+};
+
+class special_functions {
+    std::unordered_map<type::type_id, casts> _casts{};
+    std::unordered_map<std::string, unary_operator> _unary{};
+    std::unordered_map<std::string, binary_operator> _binary{};
+
+public:
+    inline auto casts(type::type_id from_type) noexcept -> casts& { return _casts[from_type]; }
+    inline auto unary(const std::string& oper) noexcept -> unary_operator& { return _unary[oper]; }
+    inline auto binary(const std::string& oper) noexcept -> binary_operator& { return _binary[oper]; }
+
+    auto new_unary(const std::string& oper, std::uint64_t precedense) noexcept -> bool;
+    auto new_binary(const std::string& oper, std::uint64_t precedense) noexcept -> bool;
 };
--- src/functions.cpp
+++ src/functions.cpp
@@ -3,64 +3,57 @@
 #include "functions.hpp"
 
 
-auto special_functions::cast_exists(type::type_id from_t, type::type_id to_t) const noexcept -> bool {
-    const auto iter = _casts.find(std::make_pair(from_t, to_t));
-    return iter != _casts.end();
-}
-
-auto special_functions::cast(type::type_id from_t, type::type_id to_t) const noexcept -> cast_inserter_wrapper {
-    if(const auto iter = _casts.find(std::make_pair(from_t, to_t)); iter != _casts.end()) {
+auto casts::get(type::type_id to_type) const -> std::optional<inserter_wrapper> {
+    if(auto iter = _casts.find(to_type); iter != _casts.end()) {
 	return iter->second;
     }
     return {};
 }
 
-void special_functions::insert_cast(type::type_id from_t, type::type_id to_t, cast_inserter_wrapper inserter) noexcept {
-    _casts[std::make_pair(from_t, to_t)] = std::move(inserter);
+void casts::insert(type::type_id to_type, inserter_wrapper inserter) {
+    _casts[to_type] = std::move(inserter);
 }
 
-auto special_functions::unary_exists(const std::string& oper, type::type_id tid) const noexcept -> bool {
-    if(auto iter = _unary.find(oper); iter != _unary.end()) {
-	const auto& op_storage = iter->second;
-	const auto op_iter = op_storage.find(tid);
-	return op_iter != op_storage.end();
-    }
-    return false;
-}
-
-auto special_functions::unary(const std::string& oper, type::type_id tid) const noexcept -> unary_inserter_wrapper {
-    if(auto iter = _unary.find(oper); iter != _unary.end()) {
-	const auto& op_storage = iter->second;
-	if(const auto op_iter = op_storage.find(tid); op_iter != op_storage.end()) {
-	    return op_iter->second;
-	}
+auto unary_operator::get(type::type_id operand_type) -> std::optional<inserter_wrapper> {
+    if(auto iter = _unary.find(operand_type); iter != _unary.end()) {
+	return iter->second;
     }
     return {};
 }
 
-void special_functions::insert_unary(const std::string& oper, type::type_id tid, unary_inserter_wrapper inserter) noexcept {
-    _unary[oper][tid] = std::move(inserter);
+void unary_operator::insert(type::type_id operand_type, inserter_wrapper inserter) {
+    _unary[operand_type] = std::move(inserter);
 }
 
-auto special_functions::binary_exists(const std::string& oper, type::type_id left, type::type_id right) const noexcept -> bool {
-    if(auto iter = _binary.find(oper); iter != _binary.end()) {
-	const auto& op_storage = iter->second;
-	const auto op_iter = op_storage.find(std::make_pair(left, right));
-	return op_iter != op_storage.end();
-    }
-    return false;
-}
-
-auto special_functions::binary(const std::string& oper, type::type_id left, type::type_id right) const noexcept -> binary_inserter_wrapper {
-    if(auto iter = _binary.find(oper); iter != _binary.end()) {
-	const auto& op_storage = iter->second;
-	if(const auto op_iter = op_storage.find(std::make_pair(left, right)); op_iter != op_storage.end()) {
-	    return op_iter->second;
-	}
+auto binary_operator::get(type::type_id left, type::type_id right) -> std::optional<inserter_wrapper> {
+    if(auto iter = _binary.find(std::make_pair(left, right)); iter != _binary.end()) {
+	return iter->second;
     }
     return {};
 }
 
-void special_functions::insert_binary(const std::string& oper, type::type_id left, type::type_id right, binary_inserter_wrapper inserter) noexcept {
-    _binary[oper][std::make_pair(left, right)] = std::move(inserter);
+void binary_operator::insert(type::type_id left, type::type_id right, inserter_wrapper inserter) {
+    _binary[std::make_pair(left, right)] = std::move(inserter);
+}
+
+auto special_functions::new_unary(const std::string& oper, std::uint64_t precedense) noexcept -> bool {
+    auto& unary_oper = _unary[oper];
+    if(unary_oper.precedense() != 0U) {
+	return false;
+    }
+
+    unary_oper = unary_operator{precedense};
+
+    return true;
+}
+
+auto special_functions::new_binary(const std::string& oper, std::uint64_t precedense) noexcept -> bool {
+    auto& binary_oper = _binary[oper];
+    if(binary_oper.precedense() != 0U) {
+	return false;
+    }
+
+    binary_oper = binary_operator{precedense};
+
+    return true;
 }
