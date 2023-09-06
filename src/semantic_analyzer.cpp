5f11bbc3503b987785a194ab44355757fe4c20c6
Le0-dot <lkolyadich@gmail.com>
2023-09-06 18:37:54 +0200

    Add implicit cast node insertion in binary expressions, return statements and function call arguments.

--- src/semantic_analyzer.cpp
+++ src/semantic_analyzer.cpp
@@ -1,16 +1,11 @@
-#include <algorithm>
-#include <any>
-#include <cassert>
-#include <iostream>
 #include <ranges>
+#include <algorithm>
 
 #include <any_tree.hpp>
-#include <typeindex>
 
-#include "any_tree/visitor.hpp"
 #include "semantic_analyzer.hpp"
 #include "tree.hpp"
-#include "type/type_id.hpp"
+
 
 auto semantic_analyzer::file(const visitor& visitor, file_node& node) -> type::type_id {
     bool result = std::ranges::all_of(
@@ -38,15 +33,27 @@ auto semantic_analyzer::function(const visitor& visitor, function_node& node) ->
     // visit statements and analyze them
     for(auto& stmt: node.children()) {
 	type::type_id tid = any_tree::visit_node(visitor, stmt);
+
+	if(!type::valid(tid)) {
+	    return type::type_id::undetermined;
+	}
+
 	auto& stmt_node = std::any_cast<statement_node&>(stmt);
 	bool invalid_return = stmt_node.payload().is_return && tid != node.payload().return_type;
 
-	if(!type::valid(tid) || invalid_return) {
-	    // try casting
+	if(!invalid_return) {
+	    continue;
+	}
+
+	auto cast = _special.cast(tid).get(node.payload().return_type);
+
+	if(!cast.has_value()) {
 	    return type::type_id::undetermined;
 	    // or
 	    // func_type = type::type_id::undetermined
 	}
+
+	stmt_node.child_at(0) = insert_impicit_cast(std::move(stmt_node.child_at(0)), node.payload().return_type);
     }
 
     return func_type;
@@ -66,36 +73,67 @@ auto semantic_analyzer::binary_expr(const visitor& visitor, binary_expr_node& no
 	return oper.first;
     }
 
-    for(const auto& [in, out]: binary_op) {
-	bool can_cast_lhs = _special.cast(lhs_type).get(in.first).has_value();
-	bool can_cast_rhs = _special.cast(rhs_type).get(in.second).has_value();
-	
-	if(can_cast_lhs && can_cast_rhs) {
-	    // insert casts
-	    return out.first;
+    for(const auto& [operands, func]: binary_op) {
+	bool should_cast_lhs = operands.first != lhs_type;
+	bool should_cast_rhs = operands.second != rhs_type;
+
+	bool can_cast_lhs = _special.cast(lhs_type).get(operands.first).has_value();
+	bool can_cast_rhs = _special.cast(rhs_type).get(operands.second).has_value();
+
+	if((should_cast_lhs && !can_cast_lhs) ||
+	   (should_cast_rhs && !can_cast_rhs)) {
+	    continue;
 	}
+
+	if(should_cast_lhs && can_cast_lhs) {
+	    insert_impicit_cast(std::move(node.child_at(0)), operands.first);
+	}
+
+	if(should_cast_rhs && can_cast_rhs) {
+	    insert_impicit_cast(std::move(node.child_at(1)), operands.second);
+	}
+	
+	return func.first;
     }
 
     return type::type_id::undetermined;
 }
 
 auto semantic_analyzer::call(const visitor& visitor, call_node& node) -> type::type_id {
-    type::type_id func_type = _scope.get(node.payload().callee);
-    if(!_types.is_function(func_type)) {
+    type::type_id func_type_id = _scope.get(node.payload().callee);
+    if(!_types.is_function(func_type_id)) {
 	return type::type_id::undetermined;
     }
     
-    bool result = std::ranges::all_of(
-	    node.children(),
-	    type::valid,
-	    std::bind_front(any_tree::visit_node<type::type_id>, visitor)
-    );
+    const type::function_type& func_type = _types.get_function(func_type_id);
 
-    if(result) {
-	return node.payload().type = _types.get_function(func_type).return_type();
+    if(node.children().size() != func_type.params().size()) {
+	return type::type_id::undetermined;
     }
 
-    return node.payload().type;
+    auto expr_iter = node.children().begin();
+    auto param_iter = func_type.params().begin();
+    for(; expr_iter != node.children().end(); ++expr_iter, ++param_iter) {
+	type::type_id expr_type = any_tree::visit_node(visitor, *expr_iter);
+
+	if(!type::valid(expr_type)) {
+	    return type::type_id::undetermined;
+	}
+
+	if(expr_type == *param_iter) {
+	    continue;
+	}
+
+	auto cast = _special.cast(expr_type).get(*param_iter);
+
+	if(!cast.has_value()) {
+	    return type::type_id::undetermined;
+	}
+
+	*expr_iter = insert_impicit_cast(std::move(*expr_iter), *param_iter);
+    }
+
+    return node.payload().type = func_type.return_type();
 }
 
 auto semantic_analyzer::identifier(identifier_node& node) -> type::type_id {
