72adce06f8f823c1d19dc02cdf004b9e0403f66d
Le0-dot <lkolyadich@gmail.com>
2023-09-08 15:23:57 +0200

    Make semantic_analyzer::binary_expr handle literals and casts properly.

--- src/semantic_analyzer.cpp
+++ src/semantic_analyzer.cpp
@@ -1,8 +1,11 @@
+#include <iostream>
 #include <ranges>
 #include <algorithm>
+#include <tuple>
 
 #include <any_tree.hpp>
 
+#include "functions.hpp"
 #include "semantic_analyzer.hpp"
 #include "tree.hpp"
 #include "type/type_id.hpp"
@@ -70,36 +73,62 @@ auto semantic_analyzer::binary_expr(const visitor& visitor, binary_expr_node& no
     type::type_id lhs_type = any_tree::visit_node(visitor, node.child_at(0));
     type::type_id rhs_type = any_tree::visit_node(visitor, node.child_at(1));
 
-    if(auto oper = binary_op.get(lhs_type, rhs_type); type::valid(oper.first)) {
-	return oper.first;
+    // try find operator with exact type definition
+    if(const auto& [type, _] = binary_op.get(lhs_type, rhs_type); type::valid(type)) {
+	node.payload().lhs = lhs_type;
+	node.payload().rhs = rhs_type;
+	return type;
     }
 
+    std::vector<std::tuple<type::type_id, type::type_id, type::type_id>> candidates{};
+
+    // add possible operators to candidate list
     for(const auto& [operands, func]: binary_op) {
 	bool should_cast_lhs = operands.first != lhs_type;
 	bool should_cast_rhs = operands.second != rhs_type;
 
+	// cast is from one type to another, not from literal to specific type
+	bool type_cast_lhs = should_cast_lhs && !type::is_literal(lhs_type);
+	bool type_cast_rhs = should_cast_rhs && !type::is_literal(rhs_type);
+
+	// allow only lhs cast or rhs cast not both
+	if(type_cast_lhs && type_cast_rhs) {
+	    continue;
+	}
+
 	bool can_cast_lhs = _special->cast(lhs_type).get(operands.first).has_value();
 	bool can_cast_rhs = _special->cast(rhs_type).get(operands.second).has_value();
 
+	// check if operand should be casted but unable to
 	if((should_cast_lhs && !can_cast_lhs) ||
 	   (should_cast_rhs && !can_cast_rhs)) {
 	    continue;
 	}
 
-	if(should_cast_lhs && can_cast_lhs) {
-	    insert_implicit_cast(std::move(node.child_at(0)), lhs_type, operands.first);
-	}
-
-	if(should_cast_rhs && can_cast_rhs) {
-	    insert_implicit_cast(std::move(node.child_at(1)), rhs_type, operands.second);
-	}
-	
-	node.payload().lhs = operands.first;
-	node.payload().rhs = operands.second;
-	return func.first;
+	candidates.emplace_back(operands.first, operands.second, func.first);
     }
 
-    return type::type_id::undetermined;
+    if(candidates.empty()) {
+	return type::type_id::undetermined;
+    }
+
+    // bring candidates with lesser literal casts to the beginning
+    // i.e lhs -> u8, lhs -> u16, ..., lhs -> fp64
+    if(type::is_literal(lhs_type)) {
+	std::ranges::sort(candidates, {}, [] (auto candidate) { return std::get<0>(candidate); });
+    }
+    // same with rhs literal
+    if(type::is_literal(rhs_type)) {
+	std::ranges::stable_sort(candidates, {}, [] (auto candidate) { return std::get<1>(candidate); });
+    }
+
+    // best candidate will be the first element
+    auto [lhs, rhs, expr] = candidates.front();
+    node.child_at(0) = insert_implicit_cast(std::move(node.child_at(0)), lhs_type, lhs);
+    node.child_at(1) = insert_implicit_cast(std::move(node.child_at(1)), rhs_type, rhs);
+    node.payload().lhs = lhs;
+    node.payload().rhs = rhs;
+    return expr;
 }
 
 auto semantic_analyzer::call(const visitor& visitor, call_node& node) -> type::type_id {
